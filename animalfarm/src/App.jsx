ad29bc5d2068a27c2fbf3f853f3740c54e02392e
NickTheKick <ntduong3@cmcglobal.vn>
2022-09-07 15:07:13 +0700

    Update App.jsx
    Complete farm animal project

--- animalfarm/src/App.jsx
+++ animalfarm/src/App.jsx
@@ -1,14 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
+  const { search, animals } = useAnimalSearch();
 
   return (
-    <div>
+    <main>
+      <h1>Animal Farm</h1>
 
-    </div>
-  )
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => search(e.target.value)}
+      />
+
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+
+        {animals.length === 0 && 'No animals found'}
+      </ul>
+    </main>
+  );
 }
 
-export default App
+// Dumb UI component
+function Animal({ type, name, age }) {
+  return (
+    <li>
+      <strong>{type}</strong> {name} ({age} years old)
+    </li>
+  );
+}
+
+// Custom Hook
+function useAnimalSearch() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem('lastQuery');
+    search(lastQuery);
+  }, []);
+
+  const search = async (q) => {
+    const response = await fetch(
+      'http://localhost:8080?' + new URLSearchParams({ q })
+    );
+    const data = await response.json();
+    setAnimals(data);
+
+    localStorage.setItem('lastQuery', q);
+  };
+
+  return { search, animals };
+}
+
+export default App;
+
